3f9a7c13721f6d3746edfe28df68980431947610
Eli <eli.sousa.2002@gmail.com>
2023-08-07 23:58:07 -0300

    feat: add new links and some itens deleted

--- src/pages/projects/index.tsx
+++ src/pages/projects/index.tsx
@@ -16,7 +16,7 @@ export const projectItem: ProjectsItem[] = [
   {
     id: 1,
     title: "Personal Profile",
-    description: "Addition, subtraction, multiplication and division tables.",
+    description: "My own personal website with projects and skills.",
     image: Todo,
     gitHub: "https://github.com/devliov/personal-profile-devliov",
     ghPages: "https://devliov.github.io/personal-profile-devliov/",
@@ -26,32 +26,23 @@ export const projectItem: ProjectsItem[] = [
     title: "To-Do List",
     description: "Your to-do tasks in an organized list.",
     image: Todo,
-    gitHub: "https://github.com/devliov/personal-profile-devliov",
-    ghPages: "https://devliov.github.io/personal-profile-devliov/",
+    gitHub: "https://github.com/devliov/todo-list-react-vite-deploy",
+    ghPages: "https://devliov.github.io/todo-list-react-vite-deploy/",
   },
   {
     id: 3,
-    title: "Coffee Delivery",
-    description:
-      "Many options of the best coffee for you and we deliver to your home.",
+    title: "Social Media",
+    description: "Basic draft of a social media feed",
     image: Todo,
-    gitHub: "https://github.com/devliov/personal-profile-devliov",
-    ghPages: "https://devliov.github.io/personal-profile-devliov/",
-  },
-  {
-    id: 4,
-    title: "Pomodoro",
-    description:
-      "  Your studies, taks and other things with time to be done and rest break.",
-    image: Todo,
-    gitHub: "https://github.com/devliov/todo-list-react-vite-deploy",
-    ghPages: "https://devliov.github.io/todo-list-react-vite-deploy/",
+    gitHub: "https://github.com/devliov/Feed-Post-Comment-react",
+    ghPages: "https://devliov.github.io/Feed-Post-Comment-react/",
   },
 ];
 
 export function Projects() {
   return (
     <ProjectContainer id="projects">
+      <h2>Projects</h2>
       {projectItem.map((item) => (
         <ProjectItem key={item.id}>
           <div>
